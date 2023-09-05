fc40e3dfdda57fd1bab70cf607ef2b9789312978
Alexis Bassignot <misterbalise2@gmail.com>
2023-09-05 14:48:27 +0200

    utilisation de use pour les autoload

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
 require "vendor\autoload.php";
-$ligue1 = new App\Championnat("Ligue 1 Uber Eats");
-$ekip1 = new App\Equipe("PSG","Louis JSP", "2015");
-$ekip2 = new App\Equipe("OM","Marcelino", "2015");
+use App\Equipe;
+use App\Championnat;
+$ligue1 = new Championnat("Ligue 1 Uber Eats");
+$ekip1 = new Equipe("PSG","Louis JSP", "2015");
+$ekip2 = new Equipe("OM","Marcelino", "2015");
 echo $ligue1 -> getNom();
 $ligue1->ajouterEquipe($ekip1);
 $ligue1->ajouterEquipe($ekip2);
