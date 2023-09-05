ced3a4b10c4fb260c2b710561e848986c527b25f
Alexis Bassignot <misterbalise2@gmail.com>
2023-09-05 15:05:15 +0200

    utilisation de namespace avec des sous dossiers

--- php-namespaces/index.php
+++ php-namespaces/index.php
@@ -2,6 +2,7 @@
 require "vendor\autoload.php";
 use App\Equipe;
 use App\Championnat;
+use App\dossier1\Personne;
 $ligue1 = new Championnat("Ligue 1 Uber Eats");
 $ekip1 = new Equipe("PSG","Louis JSP", "2015");
 $ekip2 = new Equipe("OM","Marcelino", "2015");
@@ -9,3 +10,6 @@ echo $ligue1 -> getNom();
 $ligue1->ajouterEquipe($ekip1);
 $ligue1->ajouterEquipe($ekip2);
 var_dump($ligue1->getEquipes());
+
+$personne = new Personne("HEHE BOI");
+var_dump($personne);
